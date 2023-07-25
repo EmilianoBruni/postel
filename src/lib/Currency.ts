671ac0bdce2fbbeff3a71006d0eb0906002931ed
Emiliano Bruni <info@ebruni.it>
2023-07-26 00:04:25 +0200

    Add valueOf to return original number

--- src/lib/Currency.ts
+++ src/lib/Currency.ts
@@ -1,10 +1,16 @@
 class Currency extends Number {
+    private _value: number;
     constructor(value: number) {
         super(value);
+        this._value = value;
     }
 
     public toPostelString(): string {
         return this.toFixed(2).replace('.', ',');
+    }
+
+    public valueOf(): number {
+        return this._value;
     }
 }
 
