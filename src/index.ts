214c812bc7c8265620798532035ad4140a3a9d6b
Emiliano Bruni <info@ebruni.it>
2025-01-08 13:23:37 +0100

    Add Currency and Payment exports, and update type exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import { Postel } from './lib/Postel';
+import Currency from './lib/Currency';
+import Payment from './lib/Payment';
+import { AddressParams, CommParams } from './types';
 
+export { Currency, Payment };
+export type { AddressParams, CommParams };
 export default Postel;
