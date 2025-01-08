e079fe49b70872ba62aa987b1604347543a72f65
Emiliano Bruni <info@ebruni.it>
2025-01-08 17:12:24 +0100

    Refactor index.ts to not use default export for Postel and include additional types in exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Postel } from './lib/Postel';
+import Postel from './lib/Postel';
 import Currency from './lib/Currency';
 import Payment from './lib/Payment';
-import { AddressParams, CommParams } from './types';
+import { AddressParams, CommParams, HeaderParams, BankAccount } from './types';
 
-export { Currency, Payment };
-export type { AddressParams, CommParams };
-export default Postel;
+export { Postel, Currency, Payment };
+export type { AddressParams, CommParams, HeaderParams, BankAccount };
